8497a828ea822953511b9598c9b1b66af5e6fa23
Daniil <piecyberpie@gmail.com>
2024-06-26 22:11:02 +0300

    make build

---
+++ index.a3fb3132.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,i=t.clientWidth,n=t.clientHeight;t.style.top="".concat(l/2-n/2,"px"),t.style.left="".concat(c/2-i/2,"px");
+//# sourceMappingURL=index.a3fb3132.js.map
